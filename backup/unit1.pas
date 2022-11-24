78d5153211b1aed2e9fe8d766e87d9007eef4ed6
brunaoalberto <117990229+brunaoalberto@users.noreply.github.com>
2022-11-24 08:39:18 -0300

    Initial commit

---
+++ backup/unit1.pas
@@ -0,0 +1,173 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ComCtrls,
+  DateTimePicker, AdvLed, DateUtils,
+  Unit2;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    AdvLed1: TAdvLed;
+    btnCancelar: TButton;
+    Button1: TButton;
+    btnTotalizar: TButton;
+    ckData: TCheckBox;
+    dtInicio: TDateTimePicker;
+    dtFim: TDateTimePicker;
+    Label1: TLabel;
+    Label4: TLabel;
+    Label5: TLabel;
+    Label6: TLabel;
+    Label7: TLabel;
+    Label8: TLabel;
+    lbProgressBar: TLabel;
+    lbTotalArquivos: TLabel;
+    Label3: TLabel;
+    Label2: TLabel;
+    lbTotalBruto: TLabel;
+    lbTotalAcrescimos: TLabel;
+    lbTotalDesconto: TLabel;
+    lbTotalCancelado: TLabel;
+    lbTotalLiquido: TLabel;
+    ProgressBar1: TProgressBar;
+    SelectDirectoryDialog1: TSelectDirectoryDialog;
+    procedure btnCancelarClick(Sender: TObject);
+    procedure Button1Click(Sender: TObject);
+    procedure btnTotalizarClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+
+  private
+    FRodando: Boolean;
+    procedure desativaComponentes(Sender: TObject);
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+  path: String;
+  objeto: TVerificarXmlsPasta;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+
+function ContaArquivo(Pasta, Nome:string):integer;
+var
+   Rec : TSearchRec;
+   Procura, Contador:integer;
+begin
+   Contador:=0;
+   procura :=SysUtils.FindFirst(Pasta+Nome,faAnyFile, Rec);
+   Contador:=0;
+   while procura = 0 do
+      begin
+      inc(Contador);
+      procura:=SysUtils.FindNext(Rec)
+      end;
+   SysUtils.FindClose(Rec);
+   Result:=Contador;
+end;
+
+procedure TForm1.Button1Click(Sender: TObject);
+var
+ qtdArquivos: Integer;
+begin
+  if SelectDirectoryDialog1.Execute then
+    path            := SelectDirectoryDialog1.FileName + '\';
+    AdvLed1.State   := lsOn;
+    btnTotalizar.Enabled:= True;
+    dtInicio.Enabled:= True;
+    dtFim.Enabled   := True;
+    ckData.Enabled  := True;
+    qtdArquivos     := ContaArquivo(path, 'AD*');
+    if qtdArquivos > 0 then begin
+      AdvLed1.Kind  := lkGreenLight;
+    end;
+    lbTotalArquivos.Caption:= IntToStr(qtdArquivos) + ' Arquivos';
+end;
+
+procedure TForm1.btnCancelarClick(Sender: TObject);
+begin
+  objeto.Terminate;
+end;
+
+procedure TForm1.btnTotalizarClick(Sender: TObject);
+begin
+    desativaComponentes(Sender);
+    btnCancelar.Visible:= True;
+    objeto:= TVerificarXmlsPasta.Create(true);
+    objeto.Path               := path;
+    objeto.chkBoxData         := ckData;
+    objeto.dataInicio         := dtInicio;
+    objeto.dataFim            := dtFim;
+    objeto.lbProgresso        := lbProgressBar;
+    objeto.barraProgresso     := ProgressBar1;
+    objeto.labelTotalBruto    := lbTotalBruto;
+    objeto.labelTotalDesconto := lbTotalDesconto;
+    objeto.labelTotalCancelado:= lbTotalCancelado;
+    objeto.labelTotalAcrescimo:= lbTotalAcrescimos;
+    objeto.labelTotalLiquido  := lbTotalLiquido;
+    objeto.Start;
+    objeto.OnTerminate:= @desativaComponentes;
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  dtInicio.Date:= StartOfTheMonth(IncMonth(Date, -1));
+  dtFim.Date   := EndOfTheMonth  (IncMonth(Date, -1));
+end;
+
+procedure TForm1.desativaComponentes(Sender: TObject);
+begin
+  if AdvLed1.Kind = lkYellowLight then begin
+    AdvLed1.Kind:= lkGreenLight;
+  end else begin
+    AdvLed1.Kind:= lkYellowLight
+  end;
+  btnCancelar.Visible:= False;
+  if FRodando then begin
+    FRodando:= False;
+  end else begin
+    FRodando:= True;
+  end;
+  if btnTotalizar.Enabled then begin
+    btnTotalizar.Enabled:= False;
+  end else begin
+    btnTotalizar.Enabled:= True;
+  end;
+  if Button1.Enabled then begin
+    Button1.Enabled:= False;
+  end else begin
+    Button1.Enabled:= True;
+  end;
+  if dtInicio.Enabled then begin
+    dtInicio.Enabled:= False;
+  end else begin
+    dtInicio.Enabled:= True;
+  end;
+  if dtFim.Enabled then begin
+    dtFim.Enabled:= False;
+  end else begin
+    dtFim.Enabled:= True;
+  end;
+  if ckData.Enabled then begin
+    ckData.Enabled:= False;
+  end else begin
+    ckData.Enabled:= True;
+  end;
+end;
+
+end.
+
---
+++ unit2.pas
@@ -0,0 +1,351 @@
+unit Unit2;
+
+{$mode ObjFPC}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Dialogs, StdCtrls, ComCtrls, DateTimePicker, Graphics,
+  ACBrSAT;
+
+type
+
+  { TVerificarXmlsPasta }
+
+  TVerificarXmlsPasta = Class(TThread)
+  private
+    FPath: String;
+    FMsg: String;
+    FLbProgresso: TLabel;
+    FBarraProgresso: TProgressBar;
+    FChkBoxData: TCheckBox;
+    FDataInicio: TDateTimePicker;
+    FDataFim: TDateTimePicker;
+    ACBrSAT: TACBrSAT;
+    FlabelTotalBruto: TLabel;
+    FlabelTotalLiquido: TLabel;
+    FlabelTotalDesconto: TLabel;
+    FlabelTotalCancelado: TLabel;
+    FlabelTotalAcrescimo: TLabel;
+
+    function getBarraProgresso: TProgressBar;
+    function getChkBoxData: TCheckBox;
+    function getDataFim: TDateTimePicker;
+    function getDataInicio: TDateTimePicker;
+    function getLabelTotalAcrescimo: TLabel;
+    function getLabelTotalBruto: TLabel;
+    function getLabelTotalCancelado: TLabel;
+    function getLabelTotalDesconto: TLabel;
+    function getLabelTotalLiquido: TLabel;
+    function getLbProgresso: TLabel;
+    function getPath: String;
+    procedure setBarraProgresso(AValue: TProgressBar);
+    procedure setChkBoxData(AValue: TCheckBox);
+    procedure setDataFim(AValue: TDateTimePicker);
+    procedure setDataInicio(AValue: TDateTimePicker);
+    procedure setLabelTotalAcrescimo(AValue: TLabel);
+    procedure setLabelTotalBruto(AValue: TLabel);
+    procedure setLabelTotalCancelado(AValue: TLabel);
+    procedure setLabelTotalDesconto(AValue: TLabel);
+    procedure setLabelTotalLiquido(AValue: TLabel);
+    procedure setLbProgresso(AValue: TLabel);
+    procedure setPath(AValue: String);
+
+    procedure mensagem;
+
+    procedure atualizaTela;
+
+  public
+    property Path: String read getPath write setPath;
+    property lbProgresso: TLabel read getLbProgresso write setLbProgresso;
+    property barraProgresso: TProgressBar read getBarraProgresso write setBarraProgresso;
+    property chkBoxData: TCheckBox read getChkBoxData write setChkBoxData;
+    property dataInicio: TDateTimePicker read getDataInicio write setDataInicio;
+    property dataFim: TDateTimePicker read getDataFim write setDataFim;
+    property labelTotalBruto: TLabel read getLabelTotalBruto write setLabelTotalBruto;
+    property labelTotalLiquido: TLabel read getLabelTotalLiquido write setLabelTotalLiquido;
+    property labelTotalDesconto: TLabel read getLabelTotalDesconto write setLabelTotalDesconto;
+    property labelTotalCancelado: TLabel read getLabelTotalCancelado write setLabelTotalCancelado;
+    property labelTotalAcrescimo: TLabel read getLabelTotalAcrescimo write setLabelTotalAcrescimo;
+
+    procedure Execute; override;
+
+  end;
+var
+   vlrTotalBruto    : Currency;
+   vlrTotalLiquido  : Currency;
+   vlrTotalDesconto : Currency;
+   vlrTotalCancelado: Currency;
+   vlrTotalAcrescimo: Currency;
+
+implementation
+
+{ TVerificarXmlsPasta }
+
+function ContaArquivo(Pasta, Nome:string):integer;
+var
+   Rec : TSearchRec;
+   Procura, Contador:integer;
+begin
+   Contador:=0;
+   procura :=SysUtils.FindFirst(Pasta+Nome,faAnyFile, Rec);
+   Contador:=0;
+   while procura = 0 do
+      begin
+      inc(Contador);
+      procura:=SysUtils.FindNext(Rec)
+      end;
+   SysUtils.FindClose(Rec);
+   Result:=Contador;
+end;
+
+
+function TVerificarXmlsPasta.getPath: String;
+begin
+  Result:= FPath;
+end;
+
+procedure TVerificarXmlsPasta.setBarraProgresso(AValue: TProgressBar);
+begin
+  FBarraProgresso:= AValue;
+end;
+
+procedure TVerificarXmlsPasta.setChkBoxData(AValue: TCheckBox);
+begin
+  FChkBoxData:= AValue;
+end;
+
+procedure TVerificarXmlsPasta.setDataFim(AValue: TDateTimePicker);
+begin
+  FDataFim:= AValue;
+end;
+
+procedure TVerificarXmlsPasta.setDataInicio(AValue: TDateTimePicker);
+begin
+  FDataInicio:= AValue;
+end;
+
+procedure TVerificarXmlsPasta.setLabelTotalAcrescimo(AValue: TLabel);
+begin
+  FlabelTotalAcrescimo:= AValue;
+end;
+
+procedure TVerificarXmlsPasta.setLabelTotalBruto(AValue: TLabel);
+begin
+  FlabelTotalBruto:= AValue;
+end;
+
+procedure TVerificarXmlsPasta.setLabelTotalCancelado(AValue: TLabel);
+begin
+  FlabelTotalCancelado:= AValue;
+end;
+
+procedure TVerificarXmlsPasta.setLabelTotalDesconto(AValue: TLabel);
+begin
+  FlabelTotalDesconto:= AValue;
+end;
+
+procedure TVerificarXmlsPasta.setLabelTotalLiquido(AValue: TLabel);
+begin
+  FlabelTotalLiquido:= AValue;
+end;
+
+function TVerificarXmlsPasta.getLbProgresso: TLabel;
+begin
+  Result:= FlbProgresso;
+end;
+
+function TVerificarXmlsPasta.getBarraProgresso: TProgressBar;
+begin
+  Result:= FBarraProgresso;
+end;
+
+function TVerificarXmlsPasta.getChkBoxData: TCheckBox;
+begin
+  Result:= FChkBoxData;
+end;
+
+function TVerificarXmlsPasta.getDataFim: TDateTimePicker;
+begin
+  Result:= FDataFim;
+end;
+
+function TVerificarXmlsPasta.getDataInicio: TDateTimePicker;
+begin
+  Result:= FDataInicio;
+end;
+
+function TVerificarXmlsPasta.getLabelTotalAcrescimo: TLabel;
+begin
+  result:= FlabelTotalAcrescimo;
+end;
+
+function TVerificarXmlsPasta.getLabelTotalBruto: TLabel;
+begin
+  Result:= FlabelTotalBruto;
+end;
+
+function TVerificarXmlsPasta.getLabelTotalCancelado: TLabel;
+begin
+  Result:= FlabelTotalCancelado;
+end;
+
+function TVerificarXmlsPasta.getLabelTotalDesconto: TLabel;
+begin
+  Result:= FlabelTotalDesconto;
+end;
+
+function TVerificarXmlsPasta.getLabelTotalLiquido: TLabel;
+begin
+  Result:= FlabelTotalLiquido;
+end;
+
+procedure TVerificarXmlsPasta.setLbProgresso(AValue: TLabel);
+begin
+  FlbProgresso:= AValue;
+end;
+
+procedure TVerificarXmlsPasta.setPath(AValue: String);
+begin
+  FPath:= AValue;
+end;
+
+procedure TVerificarXmlsPasta.mensagem;
+begin
+  ShowMessage(FMsg);
+end;
+
+procedure TVerificarXmlsPasta.atualizaTela;
+begin
+  FlabelTotalBruto.Caption      := FormatCurr('#,0.00', vlrTotalBruto);
+  FlabelTotalDesconto.Caption   := FormatCurr('#,0.00', vlrTotalDesconto);
+  FlabelTotalCancelado.Caption  := FormatCurr('#,0.00', vlrTotalCancelado);
+  FlabelTotalLiquido.Caption    := FormatCurr('#,0.00', vlrTotalLiquido);
+  FlabelTotalAcrescimo.Caption  := FormatCurr('#,0.00', vlrTotalAcrescimo);
+end;
+
+procedure TVerificarXmlsPasta.Execute;
+var
+  F: TSearchRec;
+  Ret: Integer;
+  i: Integer;
+begin
+  FreeOnTerminate:= True;
+  if FPath = '' then begin
+    FMsg:='Escolha a pasta com os XML''s!';
+    Synchronize( @mensagem );
+  end else begin
+    FlbProgresso.Visible     := False;
+    Ret := FindFirst(FPath + '\*.xml', faAnyFile, F);
+    try
+      vlrTotalBruto    := 0;
+      vlrTotalLiquido  := 0;
+      vlrTotalDesconto := 0;
+      vlrTotalCancelado:= 0;
+      vlrTotalAcrescimo:= 0;
+
+      FBarraProgresso.Max:= ContaArquivo(FPath, 'AD*');
+      FBarraProgresso.Min:= 0;
+      FBarraProgresso.Position:= 0;
+
+      i:= 0;
+      while Ret = 0 do begin
+        ACBrSAT:= TACBrSAT.Create(nil);
+        try
+          ACBrSAT.CFe.LoadFromFile( FPath + F.Name );
+
+          if FChkBoxData.Checked then begin
+            if ACBrSAT.CFe.ide.dEmi >= FDataInicio.Date then begin
+              if ACBrSAT.CFe.ide.dEmi < FDataFim.Date + 1 then begin
+                if Pos('ADC', F.Name) > 0 then begin
+                  vlrTotalCancelado:= vlrTotalCancelado
+                                      + ACBrSAT.CFe.Total.vCFe;
+                end;
+               vlrTotalBruto   := vlrTotalBruto
+                                  + ACBrSAT.CFe.Total.ICMSTot.vProd;
+               vlrTotalAcrescimo:= vlrTotalAcrescimo
+                                  + ACBrSAT.CFe.Total.ICMSTot.vOutro
+                                  + ACBrSAT.CFe.Total.DescAcrEntr.vAcresSubtot;
+               vlrTotalDesconto := vlrTotalDesconto
+                                  + ACBrSAT.CFe.Total.ICMSTot.vDesc
+                                  + ACBrSAT.CFe.Total.DescAcrEntr.vDescSubtot;
+              end;
+            end;
+          end else begin
+            if Pos('ADC', F.Name) > 0 then begin
+              vlrTotalCancelado:= vlrTotalCancelado
+                                  + ACBrSAT.CFe.Total.vCFe;
+            end;
+            vlrTotalBruto    := vlrTotalBruto
+                                + ACBrSAT.CFe.Total.ICMSTot.vProd;
+            vlrTotalAcrescimo:= vlrTotalAcrescimo
+                                + ACBrSAT.CFe.Total.ICMSTot.vOutro
+                                + ACBrSAT.CFe.Total.DescAcrEntr.vAcresSubtot;
+            vlrTotalDesconto:= vlrTotalDesconto
+                               + ACBrSAT.CFe.Total.ICMSTot.vDesc
+                               + ACBrSAT.CFe.Total.DescAcrEntr.vDescSubtot;
+          end;
+          if (vlrTotalBruto = 0) then begin
+
+            vlrTotalLiquido:= 0;
+          end else begin
+            vlrTotalLiquido:= vlrTotalBruto - vlrTotalCancelado - vlrTotalDesconto + vlrTotalAcrescimo;
+          end;
+          Ret := FindNext(F);
+          ACBrSAT.CFe.Clear;
+          if (i mod 5) = 0 then begin
+             Synchronize(@atualizaTela);
+             FBarraProgresso.Position:= i;
+          end;
+          i := i + 1;
+        finally
+          ACBrSAT.Free;
+        end;
+        if Terminated then begin
+           break;
+        end;
+      end;
+      if Not Terminated then begin
+        FBarraProgresso.Position   := FBarraProgresso.Max;
+        if FBarraProgresso.Position = FBarraProgresso.Max then begin
+          FLbProgresso.Parent      := FBarraProgresso;
+          FLbProgresso.Caption     := 'Carregamento completo!';
+          FLbProgresso.AutoSize    := False;
+          FLbProgresso.Transparent := True;
+          FLbProgresso.Top         :=  0;
+          FLbProgresso.Left        :=  0;
+          FLbProgresso.Width       := FBarraProgresso.ClientWidth;
+          FLbProgresso.Height      := FBarraProgresso.ClientHeight;
+          FLbProgresso.Alignment   := taCenter;
+          FLbProgresso.Layout      := tlCenter;
+          FLbProgresso.Font.Color  := clWhite;
+        end;
+      end else begin
+        vlrTotalBruto    := 0;
+        vlrTotalLiquido  := 0;
+        vlrTotalDesconto := 0;
+        vlrTotalCancelado:= 0;
+        vlrTotalAcrescimo:= 0;
+        FBarraProgresso.Position := FBarraProgresso.Max;
+        FLbProgresso.Parent      := FBarraProgresso;
+        FLbProgresso.Caption     := 'Processo cancelado!';
+        FLbProgresso.AutoSize    := False;
+        FLbProgresso.Transparent := True;
+        FLbProgresso.Top         :=  0;
+        FLbProgresso.Left        :=  0;
+        FLbProgresso.Width       := FBarraProgresso.ClientWidth;
+        FLbProgresso.Height      := FBarraProgresso.ClientHeight;
+        FLbProgresso.Alignment   := taCenter;
+        FLbProgresso.Layout      := tlCenter;
+        FLbProgresso.Font.Color  := clRed;
+      end;
+      Synchronize(@atualizaTela);
+    finally
+      FindClose(F);
+      FLbProgresso.Visible:= True;
+    end;
+  end;
+end;
+
+end.
+
